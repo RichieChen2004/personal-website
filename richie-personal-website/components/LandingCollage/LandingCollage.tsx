dab1eb3bbbb7e0f2410ab637d058f992b0bf1fca
RichieChen2004 <133861266+RichieChen2004@users.noreply.github.com>
2025-10-26 11:45:40 +1100

    Fixed scaling onladning page. Need to fix hover next

--- richie-personal-website/components/LandingCollage/LandingCollage.tsx
+++ richie-personal-website/components/LandingCollage/LandingCollage.tsx
@@ -11,90 +11,66 @@ export default function LandingCollage() {
 
   return (
     <div id="orv" className="flex items-center justify-center w-full h-screen relative bg-[white] rounded-b-2xl shadow-lg">
-      <div className={`relative w-1/2 max-w-lg aspect-[736/1308] ${ isMobile ? "-translate-x-12 translate-y-13 scale-90" : ""}`}>
+      <div className={`relative w-1/2 max-w-lg aspect-[736/1308] ${isMobile ? "scale-90 -translate-x-[18%] -translate-y-[-50%]" : "-translate-x-[0%] -translate-y-[-50%]"}`}>
+        <div className={`relative`}>
 
-        {/* Background */}
-        <div className={`${styles['origin']} -translate-x-25 -translate-y-100 ${styles['fly-in-right']}`}>
-          <Image
-            src="/dokja/zero.jpg"
-            alt="zero"
-            width={width}
-            height={height}
-            className={styles['hover-scale']}
-          />
-        </div>
+          {/* Background */}
+          <div className={`${styles['origin']} ${styles['fly-in-right']}`}>
+            <div className={styles.zero}>
+              <Image src="/dokja/zero.jpg" alt="zero" width={width} height={height} className={styles['hover-scale']} />
+            </div>
+          </div>
 
-        <div className={`${styles['origin']} translate-x-40 -translate-y-50 ${styles['fly-in-left']}`}>
-          <Image
-            src="/dokja/right face.jpg"
-            alt="right face"
-            width={width}
-            height={height}
-            className={styles['hover-scale']}
-          />
-        </div>
+          <div className={`${styles['origin']} ${styles['fly-in-left']}`}>
+            <div className={styles.rightFace}>
+              <Image src="/dokja/right face.jpg" alt="right face" width={width} height={height} className={styles['hover-scale']} />
+            </div>
+          </div>
 
-        <div className={`${styles['origin']} -translate-x-22 -translate-y-1 ${styles['fly-in-right']}`}>
-          <Image
-            src="/dokja/shoulder.jpg"
-            alt="shoulder"
-            width={width}
-            height={height}
-            className={styles['hover-scale']}
-          />
-        </div>
+          <div className={`${styles['origin']} ${styles['fly-in-right']}`}>
+            <div className={styles.shoulder}>
+              <Image src="/dokja/shoulder.jpg" alt="shoulder" width={width} height={height} className={styles['hover-scale']} />
+            </div>
+          </div>
 
-        <div className={`${styles['origin']} translate-x-35 translate-y-6 ${styles['fly-in-left']}`}>
-          <Image
-            src="/dokja/1863th.jpg"
-            alt="1863th"
-            width={width}
-            height={height}
-            className={styles['hover-scale']}
-          />
-        </div>
+          <div className={`${styles['origin']} ${styles['fly-in-left']}`}>
+            <div className={styles.img1863}>
+              <Image src="/dokja/1863th.jpg" alt="1836" width={width} height={height} className={styles['hover-scale']} />
+            </div>
+          </div>
 
+          {/* Foreground */}
 
-        {/* Foreground */}
+          <div className={`${styles['origin']} ${styles['fly-in-left']}`}>
+            <div className={styles.eye}>
+              <Image src="/dokja/eye.jpg" alt="eye" width={width} height={height} className={styles['hover-scale']} />
+            </div>
+          </div>
 
-        <div className={`${styles['origin']} -translate-x-18 -translate-y-74 ${styles['fly-in-left']}`}>
-          <Image
-            src="/dokja/eye.jpg"
-            alt="eye"
-            width={width}
-            height={height}
-            className={styles['hover-scale']}
-          />
-        </div>
+          <div className={`${styles['origin']} ${styles['fly-in-left']}`}>
+            <div className={styles.n999}>
+              <Image src="/dokja/999.jpg" alt="999" width={width} height={height} className={styles['hover-scale']} />
+            </div>
+          </div>
 
-        <div className={`${styles['origin']} -translate-x-4 translate-y-4 ${styles['fly-in-left']}`}>
-          <Image
-            src="/dokja/999.jpg"
-            alt="999"
-            width={width}
-            height={height}
-            className={styles['hover-scale']}
-          />
-        </div>
+          <div className={`${styles['origin']} ${styles['fly-in-right']}`}>
+            <div className={styles.third}>
+              <Image src="/dokja/3rd.jpg" alt="third" width={width} height={height} className={styles['hover-scale']} />
+            </div>
+          </div>
 
-        <div className={`${styles['origin']} translate-x-9.5 -translate-y-42.5 ${styles['fly-in-right']}`}>
-          <Image
-            src="/dokja/3rd.jpg"
-            alt="3rd"
-            width={width}
-            height={height}
-            className={styles['hover-scale']}
-          />
-        </div>
+          <div className={`${styles['origin']} ${styles['fly-in-right']}`}>
+            <div className={styles.nose}>
+              <Image src="/dokja/nose.jpg" alt="nose" width={width} height={height} className={styles['hover-scale']} />
+            </div>
+          </div>
 
-        <div className={`${styles['origin']} translate-x-18 translate-y-40 ${styles['fly-in-right']}`}>
-          <Image
-            src="/dokja/nose.jpg"
-            alt="nose"
-            width={width}
-            height={height}
-            className={styles['hover-scale']}
-          />
+          <div className={`${styles['origin']} ${styles['fly-in-left']}`}>
+            <div className={styles.n999}>
+              <Image src="/dokja/999.jpg" alt="999" width={width} height={height} className={styles['hover-scale']} />
+            </div>
+          </div>
+
         </div>
       </div>
     </div>
